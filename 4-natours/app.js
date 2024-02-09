e2619fc26b57be3e95c2c8f134f6907430e7ea08
SaRa Nikdel <saranikdel@SaRas-MBP.lan>
2024-02-08 20:54:35 -0800

    adding GET, POST methods

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,15 +1,47 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).json({message : 'Hello from the server side', app: 'nature' });
+// app.get('/', (req, res) => {
+//     res.status(200).json({message : 'Hello from the server side', app: 'nature' });
+// });
+
+// app.post('/', (req, res) => {
+//     res.send('You can post to this URL!');
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'suceess',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 });
 
-app.post('/', (req, res) => {
-    res.send('You can post to this URL!');
-})
+app.post('/api/v1/tours', (req, res) => {
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
